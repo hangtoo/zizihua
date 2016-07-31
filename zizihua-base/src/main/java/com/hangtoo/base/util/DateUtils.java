d2c09b73599ddaee17249c4e57cd399707b065ae
huanglf <qn_lf@163.com>
2016-07-31 11:20:40 +0800

    add job

--- zizihua-base/src/main/java/com/hangtoo/base/util/DateUtils.java
+++ zizihua-base/src/main/java/com/hangtoo/base/util/DateUtils.java
@@ -1,8 +1,31 @@
 package com.hangtoo.base.util;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DateUtils extends org.apache.commons.lang3.time.DateUtils{
+	
+	/**
+	 * 日期格式
+	 */
+	public static String pattern_d = "yyyy-MM-dd";
+
+	public static String pattern_t = "HH:mm:ss";
+	
+	public static String pattern_full = "yyyy-MM-dd HH:mm:ss";
+	
+	public static String pattern_yyyyMM="yyyyMM";
+
+	public static SimpleDateFormat shortDateFormat = new SimpleDateFormat(pattern_d);
+	
+	public static SimpleDateFormat shortTimeFormat = new SimpleDateFormat(pattern_t);
+
+	public static SimpleDateFormat fullDateFormat = new SimpleDateFormat(pattern_full);
+	
+	public static SimpleDateFormat yyyyMMDateFormat = new SimpleDateFormat(pattern_yyyyMM);
+	
 	public static Timestamp now(){
 		return getTimestamp(System.currentTimeMillis());
 	}
@@ -11,4 +34,58 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils{
 		return new java.sql.Timestamp(currentTime);
 	}
 	
+	public static Date dateNow(){
+		Calendar c = Calendar.getInstance();
+		return c.getTime();
+	}
+	
+	/**
+	 * 将所给日期转换为时间(yy-MM-dd)字符串
+	 * 
+	 * @param source
+	 *            要转换的日期对象
+	 * @return 时间字符串
+	 */
+	public static String DateToShort(Date source) {
+		String result = "";
+		result = shortDateFormat.format(source);
+		return result;
+	}
+	
+	
+	public static String convertDateToShortString(Date date){
+        String datestring = "";
+        int year = 0,month = 0,day = 0;
+      
+        if(date==null) return datestring;
+        
+        year = date.getYear()+1900;
+        month = date.getMonth()+1;
+        day = date.getDate();
+        datestring = year+"-";
+        if(month>9){
+            datestring += month+"-";
+        }else{
+            datestring += "0"+month+"-";            
+        }
+        if(day>9){
+            datestring += day;
+        }else{
+            datestring += "0"+day;            
+        }        
+        return datestring;
+    }
+	
+    //1~7分别表示周一到周日
+	public static int getWeekday(Date date){
+       Calendar gc = Calendar.getInstance();
+       gc.setTime(date);
+       int week=gc.get(Calendar.DAY_OF_WEEK);//"","星期日","星期一","星期二","星期三","星期四","星期五","星期六"
+       week--;
+		if(week==0){
+			week=7;
+		}
+		
+		return week;
+	}
 }
--- zizihua-webapp/src/main/java/hangtoo/job/stock/Job.java
+++ zizihua-webapp/src/main/java/hangtoo/job/stock/Job.java
@@ -1,28 +1,114 @@
 package hangtoo.job.stock;
 
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hangtoo.base.util.DateUtils;
+import com.hangtoo.base.util.LunarDateUtil;
+import com.hangtoo.html.EnumHeaderStyle;
+import com.hangtoo.html.decode.IHtmlDecoderFacade;
+import com.hangtoo.html.decode.impl.HtmlDecoderFacade;
+
+import hangtoo.entity.stock.TStock;
+import hangtoo.service.stock.TStockService;
 
 
 public class Job {
 	
 	private final static Logger log= Logger.getLogger(Job.class);
 	
+	@Autowired
+	private TStockService tStockService; 
+	IHtmlDecoderFacade htmlDecoderFacade=new HtmlDecoderFacade();
+	String url="http://www.szse.cn/szseWeb/FrontController.szse?ACTIONID=7&AJAX=AJAX-TRUE&CATALOGID=1803&TABKEY=tab1&txtQueryDate={QUERYDATE}&REPORT_ACTION=search";
+	String tableID="REPORTID_tab1";
+	Date now;
 	
-    public void taskCycle(){
+    public void taskCycle() throws Exception{
     	//每天执行，接口日使用统计，
     	System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
-    	synchronized(Job.class){
-    		
-			try {
+    	
+    	now=DateUtils.dateNow();
+    	log.info(now);
+    	int week=0;
+		synchronized(Job.class){
+			Date day=DateUtils.dateNow();
+			
+			while(true){
+				week=DateUtils.getWeekday(day);
+				if(week>5){//周末跳过
+					continue;
+				}
 				
-		    	
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				throw e;
+				if(day.getMonth()==0&&day.getDate()==1){//1.1跳过
+					continue;
+				}
+				if(day.getMonth()==4&&(day.getDate()>=1&&day.getDate()<=3)){//5.1跳过
+					continue;
+				}
+				if(day.getMonth()==9&&(day.getDate()>=1&&day.getDate()<=7)){//10.1跳过
+					continue;
+				}
+				
+				Date d=LunarDateUtil.solarTolunar(day);
+				if(d.getMonth()==0&&(d.getDate()>=1&&d.getDate()<=7)){
+					continue;
+				}
+				
+				this.batchSave(day);
+				Thread.sleep(15000);
 			}
-
     	}
+    }
+    
+    private void batchSave(Date day) throws Exception{
+		try {
+
+			String shortdate=DateUtils.DateToShort(day);
+			
+			url=url.replaceAll("{QUERYDATE}",shortdate);
+			List<Map<String,String>> data=htmlDecoderFacade.getTableData(url,tableID,EnumHeaderStyle.TOP);
+			
+			TStock entity;
+			String tmp=null;
+			for(Map<String,String> ele:data){
+				entity=new TStock();
+				entity.setP_date(day);
+				entity.setP_name(ele.get("指标名称"));
+				tmp=ele.get("比上日增减");
+				if(tmp!=null){
+					entity.setP_add(new BigDecimal(tmp));
+				}
+				tmp=ele.get("本日数值");
+				if(tmp!=null){
+					entity.setP_data(new BigDecimal(tmp));
+				}
+				tmp=ele.get("最高值日期");
+				if(tmp!=null){
+					entity.setP_highdate(DateUtils.parseDate(tmp, DateUtils.pattern_full));
+				}
+				tmp=ele.get("本年最高");
+				if(tmp!=null){
+					entity.setP_highdata(new BigDecimal(tmp));
+				}
+				tmp=ele.get("幅度%");
+				if(tmp!=null){
+					entity.setP_rate(new BigDecimal(tmp));
+				}
+				entity.setP_createtime(now);
+				tStockService.add(entity);
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw e;
+		}
     }
 
     
